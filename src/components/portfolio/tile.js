36c4507f7cd0e0ff99f19d70446520033e68aad5
Jacob Herper <jacob@herper.uk>
2020-07-02 10:41:57 +0100

    props destructured in tile

--- src/components/portfolio/tile.js
+++ src/components/portfolio/tile.js
@@ -1,19 +1,16 @@
 import React from "react"
 
-const Tile = (props) => {
-
-	console.log( props )
-
-	return (
-		<div id={ props.slug } className={ props.modifier ? "grid__item " + props.modifier : "grid__item" } ><a href={ "projects/" + props.slug }>
-		  <div className="grid__item-inner">
-		    <div className="grid__item-meta">
-		      <p>{ props.cat }</p>
-		    </div>
-		    <p className="grid__item-title">{ props.title }</p>
-		  </div>
-		</a></div>
-	)
-}
+const Tile = ({ slug, modifier, cat, title }) => (
+	<div id={slug} className={modifier ? "grid__item " + modifier : "grid__item"} >
+		<a href={"projects/" + slug}>
+			<div className="grid__item-inner">
+				<div className="grid__item-meta">
+					<p>{cat}</p>
+				</div>
+				<p className="grid__item-title">{title}</p>
+			</div>
+		</a>
+	</div>
+)
 
 export default Tile
